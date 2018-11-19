e6b095256408a93e2eedd2452fe9fc5e1a5ca53b
eemanuelhpe <eemanuel@hpe.com>
2018-11-19 15:09:25 +0200

    message

--- integration-tests/src/test/java/com/hp/devops/demoapp/ConfigurationService.java
+++ integration-tests/src/test/java/com/hp/devops/demoapp/ConfigurationService.java
@@ -11,6 +11,7 @@ import java.io.InterruptedIOException;
  * Testing hotspot widget by Ralu test dfgsdfh
  */
 public class ConfigurationService {
+    //Conf
 
     private static ConfigurationService instance = new ConfigurationService();
 
