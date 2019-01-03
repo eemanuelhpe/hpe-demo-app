1cab6614ce7420b2fb64d8fcd683be167abf40fb
eemanuelhpe <eemanuel@hpe.com>
2019-01-03 11:32:41 +0200

    Update DataManager.java

--- webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
+++ webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 
 /**
  * Created with IntelliJ     IDEA.
- * User: gullery 中国 test again打交道
  * Date: 24/11/14fsfsfdffdfdddgfgfgfg
  * Time: 09:58
  * To change this    template use File | Settings | File Templates.hjhjh
