4ac7228a6e4fe069fde7d9adcd7f6cb51fb78977
eemanuelhpe <eemanuel@hpe.com>
2019-01-08 11:10:56 +0200

    Update DataManager5.java

--- webapp/src/main/java/com/hp/devops/demoapp/DataManager5.java
+++ webapp/src/main/java/com/hp/devops/demoapp/DataManager5.java
@@ -89,6 +89,8 @@ public class DataManager5 {
 				output.print(json.toString(4));
 				output.close();
 				file.createNewFile();
+								file.createNewFile();
+
 	
 			}
 		} catch (IOException ioException) {
