83d9141581aa9c0e36fc4ef75bdfbb71217ef211
eemanuelhpe <eemanuel@hpe.com>
2019-01-13 15:49:50 +0200

    Update DataManager5.java

--- webapp/src/main/java/com/hp/devops/demoapp/DataManager5.java
+++ webapp/src/main/java/com/hp/devops/demoapp/DataManager5.java
@@ -16,6 +16,8 @@ import java.util.ArrayList;
  * To change this    template use File | Settings | File Templates.hjhjh
  */
 public class DataManager5 {
+	
+	
 	tatic private boolean initialized = false;
 	static private Object lock = new Object();
 	static private String resourcePath = "/WEB-INF/fdb/bands.json";
