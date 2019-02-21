d40841adae4815f4ed3afcc09c4f3422227e8496
eemanuelhpe <eemanuel@hpe.com>
2019-02-21 16:25:45 +0200

    user story #1002:

--- webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
+++ webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
  * Date: 24/11/14fsfsfdffdfdddgfgfgfg
  * Time: 09:58
  * To change this    template use File | Settings | File Templates.hjhjh
+ *
  */
 public class DataManager {
 
