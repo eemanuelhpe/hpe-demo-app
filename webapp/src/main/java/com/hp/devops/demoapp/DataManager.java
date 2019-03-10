1f55d5912e159e1017c22624274d755042e5b979
eemanuelhpe <eemanuel@hpe.com>
2019-03-10 18:15:17 +0200

    Update DataManager.java

--- webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
+++ webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
@@ -87,7 +87,8 @@ public class DataManager {
 
 				//  do i need it?
 				file.createNewFile();
-				
+								file.createNewFile();
+
 				
 				PrintWriter output = new PrintWriter(file.getAbsolutePath());
 				output.print(json.toString(3));
