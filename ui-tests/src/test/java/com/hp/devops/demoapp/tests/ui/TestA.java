e1ab3cba79c114348ff7302a4211e6717bb04c4a
eemanuelhpe <eemanuel@hpe.com>
2019-04-16 11:42:39 +0300

    Update TestA.java

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
@@ -40,7 +40,7 @@ public class TestA {
     static private String appUrl;
     
     
-    //sgfsdgfdszfdsfdsfds
+    //sgfsdgfdszfdsfdsfdsdd
 
     @BeforeClass
     static public void beforeAll() {
