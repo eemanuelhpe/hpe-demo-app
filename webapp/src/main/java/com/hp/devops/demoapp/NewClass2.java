9e65fd0529f3b88d21c4f903153bc0405986c08d
eemanuelhpe <eemanuel@hpe.com>
2019-04-30 11:39:16 +0300

    Update NewClass2.java

--- webapp/src/main/java/com/hp/devops/demoapp/NewClass2.java
+++ webapp/src/main/java/com/hp/devops/demoapp/NewClass2.java
@@ -3,7 +3,7 @@ package com.hp.devops.demoapp;
 /**
  *  Created by vaifdngato   fdfdf                                   on 05/12/2016.
  *  Testing hotspot  widget fdfdbfdfdfy Ralu test
- *  test
+ *  testd 
  */
 public class          NewClass2 {
 }
