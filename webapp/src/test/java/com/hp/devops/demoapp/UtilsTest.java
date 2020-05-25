5c2f0512c34f3881ba1bdbcf13da2540f60bd776
eemanuelhpe <eemanuel@hpe.com>
2020-05-25 19:47:22 +0300

    Update UtilsTest.java

--- webapp/src/test/java/com/hp/devops/demoapp/UtilsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/UtilsTest.java
@@ -81,7 +81,7 @@ public class UtilsTest {
 	@Test
 public void failTestForCoverageAnalysisA() {
 		String[] result = Utils.nodify("/api/some/more");
-		assertEquals(true, true);
+		assertEquals(false, true);
 	}
 
 	@Test
