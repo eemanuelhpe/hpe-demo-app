cf9245f2b3d39bbfe13798601a64f6bf3a674e1f
eemanuelhpe <eemanuel@hpe.com>
2020-06-02 02:33:34 +0300

    Update ExperimentsTest.java

--- webapp/src/test/java/com/hp/devops/demoapp/ExperimentsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/ExperimentsTest.java
@@ -60,7 +60,7 @@ public class ExperimentsTest {
 		if (randomNumber == 3) {
 			throw new IllegalMonitorStateException("this is a random exception");
 		} else {
-			Assert.assertEquals(true, true);
+			Assert.assertEquals(false, true);
 		}
 	}
 	@Test
