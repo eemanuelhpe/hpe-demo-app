3a026e3fe349cedd34c2e49bc0fc8451939b9413
eemanuelhpe <eemanuel@hpe.com>
2020-07-12 22:56:08 +0300

    user story #1007

--- webapp/src/main/java/com/hp/devops/demoapp/Utils.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Utils.java
@@ -11,14 +11,14 @@ public class Utils {
 
 	static String[] nodify(String input) {
 		ArrayList<String> nodes = new ArrayList<String>();
-		System.out.println("touching the fsds ddddddddd  faileddd test");
-		System.out.println("touching the fsdsdddddd dd  ddddddffailed test");
-		System.out.println("touching the fsdddsd ddddd  ddfdddffailed test");
-		System.out.println("touching the dfdsdsd ddd dddd faddiled test");
-		System.out.println("touching the dfssddd dddd  dfddffaddiled test");
-		System.out.println("touching the fdsdds dd dd ffdddfaiddled test");
-		System.out.println("touching the fdsdsd dddd  ffdfaddildded test");
-		System.out.println("touching the fssdd dddd d fffadiddleddd test");
+		System.out.println("touching the fsds dddddddddd  faileddd test");
+		System.out.println("touching the fsdsddddddd dd  ddddddffailed test");
+		System.out.println("touching the fsdddsd dddddd  ddfdddffailed test");
+		System.out.println("touching the dfdsdsdd ddd dddd faddiled test");
+		System.out.println("touching the dfssdddd dddd  dfddffaddiled test");
+		System.out.println("touching the fdsddsd dd dd ffdddfaiddled test");
+		System.out.println("touching the fdsdsdd dddd  ffdfaddildded test");
+		System.out.println("touching the fssdd ddddd d fffadiddleddd test");
 		System.out.println("touching the fsds d dd dfffaidldddded test");
 		System.out.println("touching the dfdss d ddff faidldded test");
 		System.out.println("touching the fsdds d dff faddidled test");
