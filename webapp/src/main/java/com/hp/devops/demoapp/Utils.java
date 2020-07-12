9837dcdd73e80613658e7c57d314c657428f94de
eemanuelhpe <eemanuel@hpe.com>
2020-07-12 22:43:13 +0300

    user story #1005

--- webapp/src/main/java/com/hp/devops/demoapp/Utils.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Utils.java
@@ -11,17 +11,17 @@ public class Utils {
 
 	static String[] nodify(String input) {
 		ArrayList<String> nodes = new ArrayList<String>();
-		System.out.println("touching the fsds dddddddd  faileddd test");
-		System.out.println("touching the fsdsddddd d  ddddddffailed test");
-		System.out.println("touching the fsddsd dddd  ddfdddffailed test");
-		System.out.println("touching the dfdssd ddd ddd faddiled test");
-		System.out.println("touching the dfssdd ddd  dfddffaddiled test");
-		System.out.println("touching the fdsds dd d ffdddfaiddled test");
-		System.out.println("touching the fdssd dddd  ffdfaddildded test");
-		System.out.println("touching the fssd ddd d fffadiddleddd test");
-		System.out.println("touching the fss d d dfffaidldddded test");
-		System.out.println("touching the dfss d dff faidldded test");
-		System.out.println("touching the fsds d dff faddidled test");
+		System.out.println("touching the fsds ddddddddd  faileddd test");
+		System.out.println("touching the fsdsdddddd d  ddddddffailed test");
+		System.out.println("touching the fsdddsd dddd  ddfdddffailed test");
+		System.out.println("touching the dfdsdsd ddd ddd faddiled test");
+		System.out.println("touching the dfssddd ddd  dfddffaddiled test");
+		System.out.println("touching the fdsdds dd d ffdddfaiddled test");
+		System.out.println("touching the fdsdsd dddd  ffdfaddildded test");
+		System.out.println("touching the fssdd ddd d fffadiddleddd test");
+		System.out.println("touching the fsds d d dfffaidldddded test");
+		System.out.println("touching the dfdss d dff faidldded test");
+		System.out.println("touching the fsdds d dff faddidled test");
 		System.out.println("touching the fss   failed test");
 		System.out.println("touching the fss   failed test");
 		System.out.println("touching the fss   failed test");
