0051a6d269540ffd6e3fc71bbe9a91048303bd5b
eemanuelhpe <eemanuel@hpe.com>
2020-07-30 10:56:18 +0300

    user story #1010, user story #1011:

--- webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
+++ webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
@@ -21,8 +21,8 @@ public class DataManager {
 	static private boolean initializedi = false;
 	static private boolean initializedi2 = false;
 	static private boolean initialized = false;
-	static private boolean initializedi1ddddd2 = false;
-	static private boolean initializedi2dd1d2 = false;
+	static private boolean initializedi1ddddddd2 = false;
+	static private boolean initializedi2dd1ddd2 = false;
 	static private boolean initialized1ddd2 = false;
 	
 	static private boolean initializedi3 = false;
