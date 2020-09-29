58fa4c6c4dfa3b401f3081a22d52097969bfa822
eemanuelhpe <eemanuel@hpe.com>
2020-09-29 17:43:44 +0300

    user story #1003:

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -13,9 +13,13 @@ import java.security.InvalidParameterException;
  */
 public class Band {
 	public int id;
+	
 	public String name = "11";
+	//
 	public String logo = "";
+	//
 	public String song = "";
+	
 	public int votes = 0;
 
 	public Band(JSONObject json) {
