a8c5d6bf339946f9e73dfd2983ce118bb29cb0de
eemanuelhpe <eemanuel@hpe.com>
2020-10-01 10:56:36 +0300

    Update DataManager.java

--- webapp/src/main/java/test/DataManager.java
+++ webapp/src/main/java/test/DataManager.java
@@ -107,7 +107,8 @@ public class DataManager {
 				file.createNewFile();
 				file.createNewFile();
 				file.createNewFile();
-				
+				file.createNewFile();
+				file.createNewFile();	
 				
 				
 				
